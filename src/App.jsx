c0eb81b3c9febe1beee43fe4fcd29421100c57bb
lbridges1 <155769050+lbridges1@users.noreply.github.com>
2024-03-18 23:47:54 +0000

    saving and pushing changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Joe Example",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "joe@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "joe-example",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
